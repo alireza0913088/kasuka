544f4d82dcd37cf7ab1666bbb3df1346acd06864
Alireza <alirezaaslane.mob7@gmail.com>
2024-08-29 15:39:08 +0330

    created top menu section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-
+import Header from './header/header'
+const body=ReactDOM.createRoot(document.getElementById("body"));
+body.render(
+    <div id='wrapper'>
+        <Header/>
+    </div>
+    
+)
 
 
